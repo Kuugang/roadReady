7f2c799d52e083c54b7852d8c2c341aa5808a59e
Kuugang <77537302+Kuugang@users.noreply.github.com>
2024-03-27 11:36:03 +0800

    google auth attempt 4

--- server/config/passport.js
+++ server/config/passport.js
@@ -6,8 +6,8 @@ module.exports = function (passport) {
     passport.use(
         new GoogleStrategy(
             {
-                clientID: process.env.CLIENT_ID,
-                clientSecret: process.env.CLIENT_SECRET,
+                clientID: process.env.GOOGLE_AUTH_CLIENT_ID,
+                clientSecret: process.env.GOOGLE_AUTH_CLIENT_SECRET,
                 callbackURL: '/auth/google/callback',
                 passReqToCallback: true
             },
